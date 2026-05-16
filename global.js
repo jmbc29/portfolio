0848ce7fe4abc4685dfd2d3f568824de99e9a100
Jimbo Cai <cjimbo29@gmail.com>
2026-05-15 23:51:32 -0700

    aaa

--- global.js
+++ global.js
@@ -93,7 +93,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
         let article = document.createElement('article');
 
         article.innerHTML = `
-            <${headingLevel}>${project.title}</${headingLevel}>
+            <${headingLevel}>${project.url 
+                ? `<a href="${project.url}" target="_blank">${project.title}</a>` 
+                : project.title
+            }</${headingLevel}>
             <img src="${project.image}" alt="${project.title}">
             <div>
                 <p>${project.description}</p>
